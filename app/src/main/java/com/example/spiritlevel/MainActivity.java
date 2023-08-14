c549f703b00cca8744aefcaa8f40d18c823b2504
Aiden Lignieres <46545209+aidenlignieres@users.noreply.github.com>
2023-08-14 17:06:46 +1000

    Fixed Monitoring Sensor Events and display

--- app/src/main/java/com/example/spiritlevel/MainActivity.java
+++ app/src/main/java/com/example/spiritlevel/MainActivity.java
@@ -15,37 +15,39 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    private SensorManager sensorManager;
+    private SensorManager manager;
     private Sensor gravitySensor;
     private SpiritLevelView spiritLevelView;
 
     @Override
-    public final void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         spiritLevelView = findViewById(R.id.spiritLevelView);
-        gravitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        gravitySensor = manager.getDefaultSensor(Sensor.TYPE_GRAVITY);
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-
-
-        List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
-
-        for (Sensor sensor: deviceSensors) {
-            Log.d("Sensors", sensor.getName());
+        List<Sensor> sensors = manager.getSensorList(Sensor.TYPE_ALL);
+        for (Sensor sensor : sensors) {
+            Log.i("MainActivity", sensor.getName());
         }
     }
 
     @Override
-    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
-        String message = String.format(Locale.getDefault(),
-                "%s: %d", sensor.getName(), accuracy);
-        Log.i("MainActivity", message);
+    protected void onResume() {
+        super.onResume();
+        manager.registerListener(this, gravitySensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
-    public final void onSensorChanged(SensorEvent event) {
+    protected void onPause() {
+        super.onPause();
+        manager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
         final String[] axes = new String[]{"x", "y", "z"};
         final String format = "%s %.2f ";
 
@@ -62,15 +64,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        sensorManager.registerListener(this, gravitySensor, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        sensorManager.unregisterListener(this);
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        String message = String.format(Locale.getDefault(),
+                "%s: %d", sensor.getName(), accuracy);
+        Log.i("MainActivity", message);
     }
 
 //    @Override
