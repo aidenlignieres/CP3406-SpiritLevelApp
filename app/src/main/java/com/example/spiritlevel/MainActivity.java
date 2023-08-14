4f1f64feab216ececa2309c4d84150bcbe5d7d7a
Aiden Lignieres <46545209+aidenlignieres@users.noreply.github.com>
2023-08-14 16:58:24 +1000

    Display list of sensors in Logcat

--- app/src/main/java/com/example/spiritlevel/MainActivity.java
+++ app/src/main/java/com/example/spiritlevel/MainActivity.java
@@ -2,13 +2,73 @@ package com.example.spiritlevel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+
+    private Sensor mLight;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public final void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        for (Sensor sensor: deviceSensors) {
+            Log.d("Sensors", sensor.getName());
+        }
     }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        // The light sensor returns a single value.
+        // Many sensors return 3 values, one for each axis.
+        float lux = event.values[0];
+        // Do something with this sensor value.
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+//
+//        List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+//
+//        for (Sensor sensor: deviceSensors) {
+//            Log.d("Sensors", sensor.getName());
+//        }
+//    }
 }
